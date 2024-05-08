44b3858fa0e7be158532bf742264d6c792e2ea48
Beyza Türken <136198282+byztrkn@users.noreply.github.com>
2024-05-08 13:54:59 +0300

    Add files via upload

---
+++ AnaEkran.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace uzayoyunuodev.Desktop
+{
+    public partial class AnaEkran : Form
+    {
+        public AnaEkran()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AnaForm frm = new AnaForm();
+            frm.ShowDialog();
+            frm.Close();
+
+        }
+
+        private void AnaEkran_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
